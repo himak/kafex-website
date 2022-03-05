b498b57c90f9c73c72c61dc15decbc2e6250b2cd
Marek Zofota <mzofota@gmail.com>
2022-03-05 16:48:06 +0100

    Change position Dots in Hero section for mobile. Add images to resources. Edit text for meta description in Home page.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'Tvorba web stránok')
 @section('meta_keywords', 'Tvorba web stránok')
-@section('meta_description', 'KAFEX - komplexné služby pre Váš web, webdesign, tvorba web stránok, e-shop, hosting, registácia domén')
+@section('meta_description', 'KAFEX - komplexné služby pre Váš web, webdizajn, tvorba web stránok, e-shop, hosting, registrácia domén')
 
 @section('content')
 
